266cb8d98fcc53b0a37baa620eb848658efa0ff3
AhmedSlman <ahmedslman151@gmail.com>
2024-02-02 16:01:58 +0200

    translation

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,20 +1,46 @@
 import assemblyai as aai
 import pyttsx3
-
+from googletrans import Translator
+import gtts
 
 aai.settings.api_key = "61341f550b5e4a64b889c0409c0f2009"
 text_speech = pyttsx3.init()
 
+
+# convirting voice to text
 
 FILE_URL = "https://github.com/AssemblyAI-Examples/audio-examples/raw/main/20230607_me_canadian_wildfires.mp3"
 
 
 transcriber = aai.Transcriber()
 transcript = transcriber.transcribe(FILE_URL)
+print(transcript.text) #printing text 
 
-print(transcript.text)
-# text_speech.say(transcript.text)
+# text_speech.say(transcript.text)#هنا بيطبق اي نص بكتبه بس اتا اديته النص اللي طلع لما حولنا الصوت 
 audio_file = "output_audio.mp3"
 text_speech.save_to_file(transcript.text, audio_file)
 text_speech.runAndWait()
 
+
+def translate_to_arabic(text):
+    translator = Translator()
+    try:
+        translation = translator.translate(text, src='en', dest='ar')
+        return translation.text
+    except Exception as e:
+        print("Translation Error:", e)
+        return None
+
+if __name__ == "__main__":
+    arabic_translation = translate_to_arabic(transcript.text)
+    if arabic_translation:
+        print("Arabic translation:", arabic_translation)
+    else:
+        print("Translation failed.")
+
+
+
+tts=gtts.gTTS(arabic_translation,lang="ar")
+tts.save("hello.mp3")
+text_speech.say(tts)
+text_speech.runAndWait()
---
+++ translate.py
@@ -0,0 +1,6 @@
+# import gtts
+# from playsound import playsound
+
+
+# tts=gtts.gTTS("Good Morninig",lang="en")
+# tts.save("hello.mp3")
